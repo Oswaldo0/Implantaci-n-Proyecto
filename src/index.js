c6a2202b85cb91ee96fa5f21369e47e0446f6d01
Oswaldo <ac20i04001@usonsonate.edu.sv>
2024-02-25 22:08:16 -0600

    Proyecto inicio

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//En este archivo es donde nosotros arrancamos nuestra aplicación 
+
+const express = require('express');
+
+
+//Inicializaciones
+const app = express();
+
+//Ajustes del servidor
+app.set('port', process.env.PORT || 4000);
+
+//Iniciar el servidor
+app.listen(app.get('port'),() =>{
+    console.log('Servidor iniciado en el puerto: ', app.get('port'));
+})
